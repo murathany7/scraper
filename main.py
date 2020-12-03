bc65d489ba44d7cf96f80870310dce28be5212b0
murathany7 <49359906+murathany7@users.noreply.github.com>
2020-12-03 19:44:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import gc
 # num to redirect to links
 num = 1
 # These are to hold Questions and Responses, respectively
-yazilar = []
-cevaplar = []
-csvrow = ['Yazi', 'Cevap']
+questions = []
+responses = []
+csvrow = ['Question', 'Response']
 # create the file
 with open('data.csv', mode='w', encoding="utf-8") as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=csvrow)
@@ -16,28 +16,27 @@ with open('data.csv', mode='w', encoding="utf-8") as csv_file:
 
 
 # method for questions with no pictures
-def resimsiz(containerss, yazi):
+def scrapNoPic(containerss, qstn):
     mesaj = containerss[0].findAll('p')
-    mesajcheck = bool(containers[0].findAll('p'))
     if len(mesaj) > 0:
         newmsj = ''
         for msj in mesaj:
             newmsj += msj.text
-        yazi[0] = str(newmsj)
+        qstn[0] = str(newmsj)
 
 
 # method to scrap for questions with pictures
-def resimli(containerss, yazi):
-    mesaj = containerss[0].findAll('td')
-    yazi[0] = str(mesaj[0].text.strip())
+def scrapwPic(container, qstn):
+    mesaj = container[0].findAll('td')
+    qstn[0] = str(mesaj[0].text.strip())
 
 
 # while loop to get all the data from 1500 pages, each page has 10 links
 while num < 1500:
-    # Create the array to pass to the CSV file, yazi array to hold questions, cevap to hold answers
+    # Create the array to pass to the CSV file, qPlaceholder array to hold questions, answer to hold answers
     topass = []
-    yazi = ['aa']
-    cevap = ''
+    qPlaceholder = ['aa']
+    answer = ''
 
     # create links to navigate
     myurl = 'https://forum.donanimhaber.com/forumid_2108/p_' + str(num) + '/tt.htm'
@@ -50,8 +49,8 @@ while num < 1500:
     # find the each link in each page
     for c in containers:
         # this is to see if the question has any answers
-        cevapsayisi = float(c.findAll('div', {'class': 'kl-cevap'})[0].string)
-        if cevapsayisi < 1:
+        answercount = float(c.findAll('div', {'class': 'kl-cevap'})[0].string)
+        if answercount < 1:
             continue
         # find the url to scrap the data
         theurl = c.findAll("div", {'class': 'kl-konu'})[0].findAll('a')[0]
@@ -62,32 +61,32 @@ while num < 1500:
             print("URL alirken sorun cikti")
             continue
         container_soup = soup(re.content, 'html.parser')
-        containerz = container_soup.findAll("span", {"class": "msg"})
+        dataContainer = container_soup.findAll("span", {"class": "msg"})
         # check if the data has picture or not
-        if containerz[0].findAll('p'):
-            resimsiz(containerz, yazi)
+        if dataContainer[0].findAll('p'):
+            scrapNoPic(dataContainer, qPlaceholder)
         else:
             try:
-                resimli(containerz)
+                scrapwPic(dataContainer)
             except:
                 continue
-        # Below is to get an answer
+        # Below is to get a response
         # check if it is the best upvoted answer or any answer, give priority to the upvoted answer
         if container_soup.findAll("div", {"id": "bestMessagesContext"}):
-            cevapcont = container_soup.findAll("div", {"id": "bestMessagesContext"})
-            cevap = cevapcont[0].findAll("div", {'class': 'icerik-content'})[0].text.strip()
+            ansCount = container_soup.findAll("div", {"id": "bestMessagesContext"})
+            answer = ansCount[0].findAll("div", {'class': 'icerik-content'})[0].text.strip()
         else:
-            # hic cevap(yorum) var mi yok mu kontrol et
+            # check if there are responses. If not, print No answer and go to next one
             try:
                 begenilmeyencevap = container_soup.findAll('span', {'class': 'msg'})
-                cevap = begenilmeyencevap[1].text.strip()
+                answer = begenilmeyencevap[1].text.strip()
             except:
-                print('Yorumsuz')
+                print('No answer')
                 continue
-        yazilar.append(yazi[0])
-        cevaplar.append(cevap)
-        topass.append({'Yazi': yazi[0], 'Cevap': cevap})
-    # append to the csv file at the end of the page
+        questions.append(qPlaceholder[0])
+        responses.append(answer)
+        topass.append({'Question': qPlaceholder[0], 'Response': answer})
+    # append question and answers as dictionary to the csv file at the end of the page
     with open('data.csv', mode='a', encoding="utf-8") as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=csvrow)
         writer.writerows(topass)
